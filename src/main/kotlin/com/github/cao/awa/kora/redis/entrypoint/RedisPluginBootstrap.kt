fb5a1636f433f0c6d8cc2b2968eae071a48762b8
cao_awa <cao.awa.www@gmail.com>
2026-05-27 09:13:21 +0800

    Remove mark plugin loaded.

--- src/main/kotlin/com/github/cao/awa/kora/redis/entrypoint/RedisPluginBootstrap.kt
+++ src/main/kotlin/com/github/cao/awa/kora/redis/entrypoint/RedisPluginBootstrap.kt
@@ -2,14 +2,12 @@ package com.github.cao.awa.kora.redis.entrypoint
 
 import com.github.cao.awa.kora.redis.client.KoraRedisClient
 import com.github.cao.awa.com.github.cao.awa.kora.redis.config.KoraRedisClientConfig
-import com.github.cao.awa.kora.plugin.markPluginLoaded
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import java.io.File
 
 object RedisPluginBootstrap {
     private val LOGGER: Logger = LogManager.getLogger("RedisPluginBootstrap")
-    val NAME: String = "kora-redis"
 
     @JvmStatic
     fun init() {
@@ -17,7 +15,5 @@ object RedisPluginBootstrap {
         val configFile = File("configs/redis_client.json")
 
         KoraRedisClient.init(KoraRedisClientConfig.createConfig(configFile))
-
-        markPluginLoaded(NAME)
     }
 }
